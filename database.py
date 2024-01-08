540e9414b13fb3abdd751e9053447e8aec0e3264
valentine-ch <valentinvchicherin@gmail.com>
2024-01-09 01:27:21 +0200

    Handle Pymongo exceptions

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from pymongo.errors import PyMongoError, ConnectionFailure, ServerSelectionTimeoutError
+from pymongo.errors import (
+    PyMongoError, ConnectionFailure, OperationFailure, ConfigurationError,
+    CursorNotFound, DuplicateKeyError, ExecutionTimeout, NetworkTimeout,
+    ServerSelectionTimeoutError, WriteError, WriteConcernError
+)
 from fastapi.exceptions import HTTPException
 
 load_dotenv()
@@ -17,8 +21,26 @@ invitations = db.invitations
 games = db.games
 
 
-def handle_db_exception(exception: PyMongoError):
-    if isinstance(exception, ConnectionFailure) or isinstance(exception, ServerSelectionTimeoutError):
-        raise HTTPException(status_code=500, detail="Could not connect to the database")
+def handle_db_exception(error: PyMongoError):
+    if isinstance(error, ConnectionFailure):
+        raise HTTPException(status_code=503, detail="Database connection failure")
+    elif isinstance(error, OperationFailure):
+        raise HTTPException(status_code=500, detail="Database operation failed")
+    elif isinstance(error, ConfigurationError):
+        raise HTTPException(status_code=500, detail="Database configuration error")
+    elif isinstance(error, CursorNotFound):
+        raise HTTPException(status_code=404, detail="Database error: Cursor not found")
+    elif isinstance(error, DuplicateKeyError):
+        raise HTTPException(status_code=409, detail="Database error: Duplicate key error")
+    elif isinstance(error, ExecutionTimeout):
+        raise HTTPException(status_code=408, detail="Database error: Execution timeout")
+    elif isinstance(error, NetworkTimeout):
+        raise HTTPException(status_code=504, detail="Database error: Network timeout")
+    elif isinstance(error, ServerSelectionTimeoutError):
+        raise HTTPException(status_code=503, detail="Database error: Server selection timeout")
+    elif isinstance(error, WriteError):
+        raise HTTPException(status_code=500, detail="Database error: Write error occurred")
+    elif isinstance(error, WriteConcernError):
+        raise HTTPException(status_code=500, detail="Database error: Write concern failed")
     else:
-        raise HTTPException(status_code=500, detail="Database error")
+        raise HTTPException(status_code=500, detail="Unknown database error")
